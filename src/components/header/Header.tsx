0feb3e108128225e4a3eb3dada614ab31d8fb890
aurilaus <akhor@nordpage.ru>
2024-05-01 22:26:06 +0300

    Initial commit

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,16 @@
 import './header.css'
 import logo from '../../wsc_trans.png';
 import {useInitData} from "@tma.js/sdk-react";
+import { HiOutlineMenu } from "react-icons/hi";
+
 function Header() {
     const initData = useInitData();
     console.log(initData?.chat)
     return (
         <div className="container">
-            <div>Приветствую, {initData?.user?.firstName} {initData?.user?.lastName}!</div>
             <img src={logo} alt="avatar" className="logo"/>
+            <div>Приветствую, {initData?.user?.firstName} {initData?.user?.lastName}!</div>
+            <HiOutlineMenu />
         </div>
     );
 }
