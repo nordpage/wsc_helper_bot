acc1726ab923e2369125e2f7226fe60c011ef955
aurilaus <akhor@nordpage.ru>
2024-05-01 17:43:19 +0300

    Initial commit

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,12 @@
 import '../App.css'
 import Header from "./header/Header.tsx";
+import { postEvent } from '@tma.js/sdk';
+import {useEffect} from "react";
 
 function App() {
+    useEffect(() => {
+        postEvent('web_app_set_header_color', { color: `#4A7272FF` });
+    }, []);
 
   return (
    <div>
