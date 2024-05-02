cfb4020ec8ebcaa4aa5f46f96e7d64125b2793b1
aurilaus <akhor@nordpage.ru>
2024-05-02 15:51:22 +0300

    Initial commit

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,13 @@
 import './footer.css'
 import about from '../../about_icon.svg'
+import documents from "../../documents_icon.svg"
 import Button from "../button/Button.tsx";
 function Footer() {
     return (
         <div className="containerFooter">
             <div className="inner">
                 <Button icon={about} title="About"/>
+                <Button icon={documents} title="Potfolio"/>
             </div>
         </div>
     );
