9c2ea267410e11535374890963d6b4fe2aea4b5c
aurilaus <akhor@nordpage.ru>
2024-05-02 13:19:06 +0300

    Initial commit

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,6 @@
 import './header.css'
 import logo from '../../wsc_trans.png';
 import {useInitData} from "@tma.js/sdk-react";
-import { HiMenu } from "react-icons/hi";
 
 function Header() {
     const initData = useInitData();
@@ -10,7 +9,6 @@ function Header() {
         <div className="containerHeader">
             <img src={logo} alt="avatar" className="logo"/>
             <div>Приветствую, {initData?.user?.firstName} {initData?.user?.lastName}!</div>
-            <HiMenu size="32px"/>
         </div>
     );
 }
